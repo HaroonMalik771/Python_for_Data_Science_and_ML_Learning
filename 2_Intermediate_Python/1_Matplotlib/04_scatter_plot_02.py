0652f1a0a1dc89b5415f9c3a8732d4e89da60689
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-07-10 16:13:02 +0500

    update

---
+++ 2_Intermediate_Python/1_Matplotlib/04_scatter_plot_02.py
@@ -0,0 +1,17 @@
+# Import package
+
+
+
+pop = [2.53, 2.57, 2.62, 2.67, 2.71]
+
+life_exp = [43.828, 76.423, 72.301, 42.731,45.678]
+
+import matplotlib.pyplot as plt
+
+
+# Build Scatter plot
+
+plt.scatter(pop,life_exp)
+
+# Show plot
+plt.show()
