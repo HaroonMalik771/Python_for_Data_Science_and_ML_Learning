dcb8fbd679702010168ca51fe29df75d10053b2f
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-07-15 13:24:36 +0500

    update

---
+++ 2_Intermediate_Python/5_Case_study_Hacker_Statistics/02-roll-the-dice.py
@@ -0,0 +1,14 @@
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+
+number1 = np.random.randint(1,7)
+
+print(number1)
+
+# Use randint() again
+
+number2 = np.random.randint(1,7)
+print(number2)
