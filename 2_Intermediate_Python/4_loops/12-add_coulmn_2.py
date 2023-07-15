9869a667bdc2edcbe5120dc7322ab387217e8f71
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-07-15 13:04:26 +0500

    update

---
+++ 2_Intermediate_Python/4_loops/12-add_coulmn_2.py
@@ -0,0 +1,8 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('D:/Python/Python_for_Data_Science_and_ML_Learning/2_Intermediate_Python/4_loops/cars.csv', index_col = 0)
+
+# Use .apply(str.upper)
+for lab, row in cars.iterrows() :
+   cars.loc[:, "COUNTRY"] = cars["country"].apply(str.upper)
+   print(cars)
