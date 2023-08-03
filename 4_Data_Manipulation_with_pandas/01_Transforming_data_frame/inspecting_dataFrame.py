553bd45f39ee274eeb729a00ed6baa1c1e18be2b
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-08-03 15:04:48 +0500

    update

--- 4_Data_Manipulation_with_pandas/01_Transforming_data_frame/inspecting_dataFrame.py
+++ 4_Data_Manipulation_with_pandas/01_Transforming_data_frame/inspecting_dataFrame.py
@@ -2,8 +2,14 @@ import pandas as pd
 
 # Print the head of the homelessness data
 
-homelessness = pd.read_csv("D:/Python\Python_for_Data_Science_and_ML_Learning/4_Data_Manipulation_with_pandas/1-Transforming_data_frame/homelessness.csv")
+homelessness = pd.read_csv("D:/Python/Python_for_Data_Science_and_ML_Learning/4_Data_Manipulation_with_pandas/01_Transforming_data_frame/homelessness.csv", index_col=0, header=0)
 
 print(homelessness.head())
 # Print information about homelessness
 print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
