7b8ed529eb84879197780da35bb2c0651caed437
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-08-05 23:25:40 +0500

    update

---
+++ 4_Data_Manipulation_with_pandas/01_Transforming_data_frame/05_subsetting_rows.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+homelessness = pd.read_csv("D:/Python/Python_for_Data_Science_and_ML_Learning/4_Data_Manipulation_with_pandas/01_Transforming_data_frame/homelessness.csv", index_col=0, header=0)
+
+# Filter for rows where individuals is greater than 10000
+ind_gt_10k = homelessness[homelessness["individuals"] > 1000]
+
+# See the result
+print(ind_gt_10k)
