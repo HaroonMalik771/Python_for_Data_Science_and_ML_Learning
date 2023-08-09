7e163a35cad1462c4b0703d524d96bb82be641e0
Muhammad Haroon Shahzad <129644841+HaroonMalik771@users.noreply.github.com>
2023-08-09 23:50:28 +0500

    Update 01_inspecting_dataFrame.py

--- 4_Data_Manipulation_with_pandas/01_Transforming_data_frame/01_inspecting_dataFrame.py
+++ 4_Data_Manipulation_with_pandas/01_Transforming_data_frame/01_inspecting_dataFrame.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Print the head of the homelessness data
+# Print the head of the homelessness Data
 
 homelessness = pd.read_csv("D:/Python/Python_for_Data_Science_and_ML_Learning/4_Data_Manipulation_with_pandas/01_Transforming_data_frame/homelessness.csv", index_col=0, header=0)
 
